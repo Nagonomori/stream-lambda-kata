8ab81c0db951042bc7a7422fd6e05795383932dd
Rene Esemann <rene.esemann@kv.digital>
2021-05-14 21:19:16 +0200

    Zum Test mal "oldschool" implementiert. Stream-API ist aufjedenfall bequemer wenn man weiß wie es geht

--- src/main/java/com/github/jorgenringen/lambda/stream/Util.java
+++ src/main/java/com/github/jorgenringen/lambda/stream/Util.java
@@ -1,50 +1,142 @@
 package com.github.jorgenringen.lambda.stream;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import java.util.IntSummaryStatistics;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Util {
+    private Util() {
+
+    }
 
     public static List<String> mapToUppercase(List<String> input) {
-        throw new NotImplementedException();    }
+        ArrayList<String> strings = new ArrayList<>();
+        for (String s : input) {
+            strings.add(s.toUpperCase());
+        }
+        return strings;
+    }
 
     public static List<String> removeElementsWithMoreThanFourCharacters(List<String> input) {
-        throw new NotImplementedException();    }
+        ArrayList<String> strings = new ArrayList<>();
+        for (String s : input) {
+            if (s.length() < 4) {
+                strings.add(s);
+            }
+        }
+        return strings;
+    }
 
     public static List<String> sortStrings(List<String> input) {
-        throw new NotImplementedException();    }
+        String[] strings = input.toArray(input.toArray(new String[0]));
+        Arrays.sort(strings);
+        return Arrays.asList(strings);
+    }
 
     public static List<Integer> sortIntegers(List<String> input) {
-        throw new NotImplementedException();    }
+        Integer[] integers = new Integer[input.size()];
+        int i = 0;
+        for (String s : input) {
+            integers[i] = Integer.parseInt(s);
+            i++;
+        }
+        Arrays.sort(integers);
+        return Arrays.asList(integers);
+    }
 
     public static List<Integer> sortIntegersDescending(List<String> input) {
-        throw new NotImplementedException();    }
+        Integer[] integers = new Integer[input.size()];
+        int i = 0;
+        for (String s : input) {
+            integers[i] = Integer.parseInt(s);
+            i++;
+        }
+        Arrays.sort(integers, Comparator.reverseOrder());
+        return Arrays.asList(integers);
+    }
 
     public static Integer sum(List<Integer> numbers) {
-        throw new NotImplementedException();    }
+        Integer i = 0;
+        for (Integer number : numbers) {
+            i += number;
+        }
+        return i;
+    }
 
     public static List<String> flattenToSingleCollection(List<List<String>> input) {
-        throw new NotImplementedException();
+        ArrayList<String> strings = new ArrayList<>();
+        for (List<String> list : input) {
+            strings.addAll(list);
+        }
+        return strings;
     }
 
     public static String separateNamesByComma(List<Person> input) {
-        throw new NotImplementedException();    }
+        StringBuilder line = new StringBuilder("Names: ");
+        for (Person p : input) {
+            line.append(p.getName()).append(", ");
+        }
+        line = new StringBuilder(line.substring(0, line.length() - 2) + ".");
+        return line.toString();
+    }
 
     public static String findNameOfOldestPerson(List<Person> input) {
-        throw new NotImplementedException();    }
+        Person oldest = input.get(0);
+        for (Person p : input) {
+            if (p.getAge() > oldest.getAge()) {
+                oldest = p;
+            }
+        }
+        return oldest.getName();
+    }
 
     public static List<String> filterPeopleLessThan18YearsOld(List<Person> input) {
-        throw new NotImplementedException();    }
+        ArrayList<String> kids = new ArrayList<>();
+        for (Person p : input) {
+            if (p.getAge() < 18) {
+                kids.add(p.getName());
+            }
+        }
+        return kids;
+    }
 
     public static IntSummaryStatistics getSummaryStatisticsForAge(List<Person> input) {
-        throw new NotImplementedException();    }
+        IntSummaryStatistics intSummaryStatistics = new IntSummaryStatistics();
+        for (Person p : input) {
+            intSummaryStatistics.accept(p.getAge());
+        }
+
+        return intSummaryStatistics;
+    }
 
     public static Map<Boolean, List<Person>> partitionAdults(List<Person> input) {
-        throw new NotImplementedException();    }
+        HashMap<Boolean, List<Person>> map = new HashMap<>();
+        ArrayList<Person> adultList = new ArrayList<>();
+        ArrayList<Person> kidList = new ArrayList<>();
+        for (Person p : input) {
+            if (p.getAge() >= 18) {
+                adultList.add(p);
+            } else {
+                kidList.add(p);
+            }
+        }
+        map.put(true, adultList);
+        map.put(false, kidList);
+        return map;
+    }
 
     public static Map<String, List<Person>> partitionByNationality(List<Person> input) {
-        throw new NotImplementedException();    }
+        HashMap<String, List<Person>> map = new HashMap<>();
+        for (Person p : input) {
+            String country = p.getCountry();
+            if (map.containsKey(country)) {
+                List<Person> people = map.get(country);
+                people.add(p);
+                map.put(country, people);
+            } else {
+                ArrayList<Person> people = new ArrayList<>();
+                people.add(p);
+                map.put(country, people);
+            }
+        }
+        return map;
+    }
 }
